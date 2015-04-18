8f44ee539177354256dfcd027ec2af3c588c72c0
Arthur Guy <arthur@assys.net>
2015-04-18 13:17:06 +0100

    refactored message saving

--- app/Http/Controllers/MessageController.php
+++ app/Http/Controllers/MessageController.php
@@ -1,26 +1,54 @@
 <?php namespace App\Http\Controllers;
 
+use Carbon\Carbon;
 use Illuminate\Http\Request;
 
-class MessageController extends Controller {
+class MessageController extends Controller
+{
 
     /**
      * @param Request $message
      */
     public function save(Request $message)
     {
-        $user = $message->get('user_name');
+        $user    = $message->get('user_name');
         $channel = $message->get('channel_name');
-        $time = $message->get('timestamp');
-        $text = $message->get('text');
+        $text    = $message->get('text');
+        $domain  = $message->get('team_domain');
 
-        $archiveMessage = $user.' | '.$text.' | '.$time;
+        $date = $this->extractDateTimeFromRequest($message);
 
-        if (!\Storage::exists($channel.'.txt')) {
-            \Storage::put($channel.'.txt', $archiveMessage);
+        //$archiveMessage = $user . ' | ' . $text . ' | ' . $date->format('Y-m-d H:i:s');
+
+        $archiveMessage = [
+            'user'    => $user,
+            'message' => $text,
+            'date'    => $date->format('Y-m-d H:i:s'),
+            'domain'  => $domain,
+        ];
+
+        $messageToSave = json_encode($archiveMessage);
+
+        $fileName = $channel . '.txt';
+
+        if (!\Storage::exists($fileName)) {
+            \Storage::put($fileName, $messageToSave);
         } else {
-            \Storage::prepend($channel.'.txt', $archiveMessage);
+            \Storage::prepend($fileName, $messageToSave);
         }
+    }
+
+    /**
+     * @param Request $message
+     * @return Carbon
+     */
+    private function extractDateTimeFromRequest(Request $message)
+    {
+        $time = $message->get('timestamp');
+        list($timestamp, $miliseconds) = explode('.', $time);
+        $date = Carbon::createFromTimestamp($timestamp);
+
+        return $date;
     }
 
 }
